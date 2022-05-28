d9f087a4424f86d4769d5d996d4f92603fa9372f
Jason Bracco <bracco.jason@gmail.com>
2022-05-28 13:42:50 -0500

    commit tree 2885f5239c2da9678154caec89fe1d85ce0b560e

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
-function hasTargetSum(array, target) {
-  // Write your algorithm here
+function hasTargetSum(array, target){
+  for(i=0;i<array.length-1;i++){
+    for(j=(i+1);j<array.length;j++){
+
+      if (array[i]+array[j] === target){
+      return true
+
+      }
+    }
+  }
+  return false
 }
 
 /* 
   Write the Big O time complexity of your function here
+  iterating through the array with i = n
+  iterating through the array with j = n
+  n*n = n^2
 */
 
 /* 
-  Add your pseudocode here
+  Take in one array and a target value
+  iterate over the given array 
+  if a value on index i + a value on index j = the target value
+  return true
 */
 
 /*
@@ -20,15 +35,15 @@ if (require.main === module) {
   console.log("Expecting: true");
   console.log("=>", hasTargetSum([3, 8, 12, 4, 11, 7], 10));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: true");
-  console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
+  // console.log("Expecting: true");
+  // console.log("=>", hasTargetSum([22, 19, 4, 6, 30], 25));
 
-  console.log("");
+  // console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", hasTargetSum([1, 2, 5], 4));
+  // console.log("Expecting: false");
+  // console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
 
 module.exports = hasTargetSum;
